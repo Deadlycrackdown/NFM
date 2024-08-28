2ccac8ed7173b510e7aadae6d737f185e47cb426
Deadlycrackdown <ramisleet@gmail.com>
2024-08-28 16:34:47 +0300

    first commit

---
+++ NetFolderManager/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿
+namespace NetFolderManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.addButton = new MetroFramework.Controls.MetroButton();
+            this.networkFoldersListBox = new System.Windows.Forms.ListBox();
+            this.folderPathTextBox = new MetroFramework.Controls.MetroTextBox();
+            this.usernameTextBox = new MetroFramework.Controls.MetroTextBox();
+            this.passwordTextBox = new MetroFramework.Controls.MetroTextBox();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel3 = new MetroFramework.Controls.MetroLabel();
+            this.importButton = new MetroFramework.Controls.MetroButton();
+            this.metroLabel4 = new MetroFramework.Controls.MetroLabel();
+            this.nameLabel = new MetroFramework.Controls.MetroLabel();
+            this.timerLabelPrint = new System.Windows.Forms.Timer(this.components);
+            this.DuranInvisibleCheckBox = new System.Windows.Forms.CheckBox();
+            this.clearAllButton = new MetroFramework.Controls.MetroButton();
+            this.autorLabel = new MetroFramework.Controls.MetroLabel();
+            this.metroToolTip = new MetroFramework.Components.MetroToolTip();
+            this.deleteButton = new MetroFramework.Controls.MetroButton();
+            this.SuspendLayout();
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(359, 230);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(177, 23);
+            this.addButton.TabIndex = 0;
+            this.addButton.Text = "Добавить";
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // networkFoldersListBox
+            // 
+            this.networkFoldersListBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.networkFoldersListBox.FormattingEnabled = true;
+            this.networkFoldersListBox.ItemHeight = 20;
+            this.networkFoldersListBox.Location = new System.Drawing.Point(23, 91);
+            this.networkFoldersListBox.Name = "networkFoldersListBox";
+            this.networkFoldersListBox.Size = new System.Drawing.Size(284, 284);
+            this.networkFoldersListBox.TabIndex = 1;
+            this.networkFoldersListBox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.networkFoldersListBox_MouseDoubleClick);
+            // 
+            // folderPathTextBox
+            // 
+            this.folderPathTextBox.Location = new System.Drawing.Point(359, 91);
+            this.folderPathTextBox.Name = "folderPathTextBox";
+            this.folderPathTextBox.Size = new System.Drawing.Size(177, 23);
+            this.folderPathTextBox.TabIndex = 2;
+            // 
+            // usernameTextBox
+            // 
+            this.usernameTextBox.Location = new System.Drawing.Point(359, 139);
+            this.usernameTextBox.Name = "usernameTextBox";
+            this.usernameTextBox.Size = new System.Drawing.Size(177, 23);
+            this.usernameTextBox.TabIndex = 3;
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(359, 187);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(177, 23);
+            this.passwordTextBox.TabIndex = 4;
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(359, 69);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(147, 19);
+            this.metroLabel1.TabIndex = 5;
+            this.metroLabel1.Text = "Путь до сетевой папки";
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.Location = new System.Drawing.Point(359, 117);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(122, 19);
+            this.metroLabel2.TabIndex = 6;
+            this.metroLabel2.Text = "Имя пользователя";
+            // 
+            // metroLabel3
+            // 
+            this.metroLabel3.AutoSize = true;
+            this.metroLabel3.Location = new System.Drawing.Point(359, 165);
+            this.metroLabel3.Name = "metroLabel3";
+            this.metroLabel3.Size = new System.Drawing.Size(54, 19);
+            this.metroLabel3.TabIndex = 7;
+            this.metroLabel3.Text = "Пароль";
+            // 
+            // importButton
+            // 
+            this.importButton.Location = new System.Drawing.Point(358, 352);
+            this.importButton.Name = "importButton";
+            this.importButton.Size = new System.Drawing.Size(177, 23);
+            this.importButton.TabIndex = 8;
+            this.importButton.Text = "Импорт данных[Эксперт]";
+            this.importButton.Visible = false;
+            this.importButton.Click += new System.EventHandler(this.importButton_Click);
+            // 
+            // metroLabel4
+            // 
+            this.metroLabel4.AutoSize = true;
+            this.metroLabel4.Location = new System.Drawing.Point(74, 69);
+            this.metroLabel4.Name = "metroLabel4";
+            this.metroLabel4.Size = new System.Drawing.Size(172, 19);
+            this.metroLabel4.TabIndex = 9;
+            this.metroLabel4.Text = "Добавленные соединения";
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(74, 32);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(0, 0);
+            this.nameLabel.TabIndex = 10;
+            // 
+            // DuranInvisibleCheckBox
+            // 
+            this.DuranInvisibleCheckBox.AutoSize = true;
+            this.DuranInvisibleCheckBox.Location = new System.Drawing.Point(359, 381);
+            this.DuranInvisibleCheckBox.Name = "DuranInvisibleCheckBox";
+            this.DuranInvisibleCheckBox.Size = new System.Drawing.Size(111, 17);
+            this.DuranInvisibleCheckBox.TabIndex = 11;
+            this.DuranInvisibleCheckBox.Text = "Режим эксперта";
+            this.DuranInvisibleCheckBox.UseVisualStyleBackColor = true;
+            this.DuranInvisibleCheckBox.Visible = false;
+            this.DuranInvisibleCheckBox.CheckedChanged += new System.EventHandler(this.DuranInvisibleCheckBox_CheckedChanged);
+            // 
+            // clearAllButton
+            // 
+            this.clearAllButton.Location = new System.Drawing.Point(358, 323);
+            this.clearAllButton.Name = "clearAllButton";
+            this.clearAllButton.Size = new System.Drawing.Size(177, 23);
+            this.clearAllButton.TabIndex = 12;
+            this.clearAllButton.Text = "Очистить все соединения";
+            this.clearAllButton.Visible = false;
+            this.clearAllButton.Click += new System.EventHandler(this.clearAllButton_Click);
+            // 
+            // autorLabel
+            // 
+            this.autorLabel.AutoSize = true;
+            this.autorLabel.Location = new System.Drawing.Point(23, 411);
+            this.autorLabel.Name = "autorLabel";
+            this.autorLabel.Size = new System.Drawing.Size(201, 19);
+            this.autorLabel.TabIndex = 13;
+            this.autorLabel.Text = "developed by Nuriev R. (ver. 1.01)";
+            this.autorLabel.Visible = false;
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(359, 294);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(177, 23);
+            this.deleteButton.TabIndex = 14;
+            this.deleteButton.Text = "Удалить";
+            this.deleteButton.Visible = false;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BorderStyle = MetroFramework.Drawing.MetroBorderStyle.FixedSingle;
+            this.ClientSize = new System.Drawing.Size(568, 450);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.autorLabel);
+            this.Controls.Add(this.clearAllButton);
+            this.Controls.Add(this.DuranInvisibleCheckBox);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.metroLabel4);
+            this.Controls.Add(this.importButton);
+            this.Controls.Add(this.metroLabel3);
+            this.Controls.Add(this.metroLabel2);
+            this.Controls.Add(this.metroLabel1);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.usernameTextBox);
+            this.Controls.Add(this.folderPathTextBox);
+            this.Controls.Add(this.networkFoldersListBox);
+            this.Controls.Add(this.addButton);
+            this.Name = "Form1";
+            this.Resizable = false;
+            this.Style = MetroFramework.MetroColorStyle.Black;
+            this.Text = "NFM";
+            this.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroButton addButton;
+        private System.Windows.Forms.ListBox networkFoldersListBox;
+        private MetroFramework.Controls.MetroTextBox folderPathTextBox;
+        private MetroFramework.Controls.MetroTextBox usernameTextBox;
+        private MetroFramework.Controls.MetroTextBox passwordTextBox;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private MetroFramework.Controls.MetroLabel metroLabel3;
+        private MetroFramework.Controls.MetroButton importButton;
+        private MetroFramework.Controls.MetroLabel metroLabel4;
+        private MetroFramework.Controls.MetroLabel nameLabel;
+        private System.Windows.Forms.Timer timerLabelPrint;
+        private System.Windows.Forms.CheckBox DuranInvisibleCheckBox;
+        private MetroFramework.Controls.MetroButton clearAllButton;
+        private MetroFramework.Controls.MetroLabel autorLabel;
+        private MetroFramework.Components.MetroToolTip metroToolTip;
+        private MetroFramework.Controls.MetroButton deleteButton;
+    }
+}
+
---
+++ NetFolderManager/Form1.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+using Newtonsoft.Json;
+
+namespace NetFolderManager
+{
+    public partial class Form1 : MetroForm
+    {
+        private List<NetworkConnection> networkConnections = new List<NetworkConnection>();
+        private int currentIndex = 0;
+        private const string labelText = "Network Folder Manager";
+        private const string NetworkConnectionsFile = "network_connections.json";
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeTimers();
+            InitializeTooltips();
+        }
+
+        private void InitializeTimers()
+        {
+            timerLabelPrint.Tick += TimerLabelPrint_Tick;
+            timerLabelPrint.Interval = 100;
+            timerLabelPrint.Start();
+        }
+
+        private void InitializeTooltips()
+        {
+            metroToolTip.SetToolTip(addButton, "Нажмите, чтобы добавить новое сетевое соединение");
+            metroToolTip.SetToolTip(folderPathTextBox, "Пример: \\\\число.число.число.число\\имя_папки");
+            metroToolTip.SetToolTip(usernameTextBox, "Логин от сетевого соединения (Это не логин 1С и учетной записи Windows)");
+            metroToolTip.SetToolTip(passwordTextBox, "Пароль от сетевого соединения (Это не пароль от вашей учетной записи)");
+        }
+
+        private void TimerLabelPrint_Tick(object sender, EventArgs e)
+        {
+            if (currentIndex < labelText.Length)
+            {
+                nameLabel.Text += labelText[currentIndex];
+                currentIndex++;
+            }
+            else
+            {
+                timerLabelPrint.Stop();
+            }
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            string folderPath = folderPathTextBox.Text.Trim();
+            string username = usernameTextBox.Text.Trim();
+            string password = passwordTextBox.Text.Trim();
+
+            if (string.IsNullOrWhiteSpace(folderPath) || string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
+            {
+                MessageBox.Show("Пожалуйста, заполните все поля для добавления сетевой папки.", "Предупреждение", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (networkConnections.Any(connection => connection.FolderPath.Equals(folderPath, StringComparison.OrdinalIgnoreCase) &&
+                                                      connection.Username.Equals(username, StringComparison.OrdinalIgnoreCase)))
+            {
+                MessageBox.Show("Эта сетевая папка уже добавлена с этими учетными данными.", "Предупреждение", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            NetworkConnection newConnection = new NetworkConnection(folderPath, username, password);
+            networkConnections.Add(newConnection);
+
+            networkFoldersListBox.Items.Add(newConnection.ToString());
+
+            folderPathTextBox.Clear();
+            usernameTextBox.Clear();
+            passwordTextBox.Clear();
+
+            SaveNetworkConnections();
+        }
+
+        private void networkFoldersListBox_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (networkFoldersListBox.SelectedIndex != -1)
+            {
+                NetworkConnection selectedConnection = networkConnections[networkFoldersListBox.SelectedIndex];
+                ConnectToNetworkFolder(selectedConnection);
+            }
+        }
+
+        private void ConnectToNetworkFolder(NetworkConnection connection)
+        {
+            if (IsNetworkFolderOpen(connection.FolderPath))
+            {
+                DialogResult result = MessageBox.Show(
+                    "Данное сетевое расположение уже открыто. Необходимо закрыть предыдущее расположение перед открытием нового. Хотите закрыть текущее расположение?",
+                    "Предупреждение",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Warning);
+
+                if (result == DialogResult.Yes)
+                {
+                    CloseNetworkFolder(connection.FolderPath);
+                    OpenNetworkFolder(connection);
+                }
+            }
+            else
+            {
+                OpenNetworkFolder(connection);
+            }
+        }
+
+        private bool IsNetworkFolderOpen(string folderPath)
+        {
+            var cmd = $"/c net use | find \"{folderPath}\"";
+            return ExecuteCommand(cmd) == 0;
+        }
+
+        private void OpenNetworkFolder(NetworkConnection connection)
+        {
+            var cmd = $"/c net use {connection.FolderPath} /user:{connection.Username} {connection.Password} /persistent:yes";
+            ExecuteCommand(cmd);
+
+            cmd = $"/c explorer \"{connection.FolderPath}\"";
+            ExecuteCommand(cmd);
+        }
+
+        private void CloseNetworkFolder(string folderPath)
+        {
+            var cmd = $"/c net use /delete /y \"{folderPath}\"";
+            ExecuteCommand(cmd);
+        }
+
+        private void RemoveAllNetworkConnections()
+        {
+            foreach (NetworkConnection connection in networkConnections)
+            {
+                CloseNetworkFolder(connection.FolderPath);
+            }
+        }
+
+        private void DuranInvisibleCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            bool isVisible = DuranInvisibleCheckBox.Checked;
+            clearAllButton.Visible = isVisible;
+            importButton.Visible = isVisible;
+            autorLabel.Visible = isVisible;
+        }
+
+        private void importButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*",
+                Title = "Выберите файл для импорта"
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog.FileName;
+
+                try
+                {
+                    string[] lines = File.ReadAllLines(filePath);
+
+                    foreach (string line in lines)
+                    {
+                        string[] parts = line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                        if (parts.Length >= 3)
+                        {
+                            string folderPath = parts[0];
+                            string username = parts[1];
+                            string password = parts[2];
+
+                            NetworkConnection newConnection = new NetworkConnection(folderPath, username, password);
+
+                            if (!networkConnections.Any(connection => connection.FolderPath.Equals(folderPath, StringComparison.OrdinalIgnoreCase) &&
+                                                                       connection.Username.Equals(username, StringComparison.OrdinalIgnoreCase)))
+                            {
+                                networkConnections.Add(newConnection);
+                                networkFoldersListBox.Items.Add(newConnection.ToString());
+                            }
+                            else
+                            {
+                                MessageBox.Show($"Сетевая папка {folderPath} уже добавлена с этими учетными данными.", "Предупреждение", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show($"Неправильный формат строки: {line}. Строка должна быть в формате: FOLDER NAME PASSWORD", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+
+                    MessageBox.Show("Сетевые папки успешно импортированы.", "Импорт завершен", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка при импорте сетевых папок: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+                SaveNetworkConnections();
+            }
+        }
+
+        private void clearAllButton_Click(object sender, EventArgs e)
+        {
+            networkConnections.Clear();
+            networkFoldersListBox.Items.Clear();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.D5)
+            {
+                DuranInvisibleCheckBox.Visible = true;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.KeyPreview = true;
+            this.KeyDown += Form1_KeyDown;
+            LoadNetworkConnections();
+        }
+
+        private void SaveNetworkConnections()
+        {
+            try
+            {
+                string json = JsonConvert.SerializeObject(networkConnections, Formatting.Indented);
+                File.WriteAllText(NetworkConnectionsFile, json);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при сохранении сетевых подключений: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void LoadNetworkConnections()
+        {
+            try
+            {
+                if (File.Exists(NetworkConnectionsFile))
+                {
+                    string json = File.ReadAllText(NetworkConnectionsFile);
+                    networkConnections = JsonConvert.DeserializeObject<List<NetworkConnection>>(json) ?? new List<NetworkConnection>();
+
+                    foreach (NetworkConnection connection in networkConnections)
+                    {
+                        networkFoldersListBox.Items.Add(connection.ToString());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при загрузке сетевых подключений: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private int ExecuteCommand(string cmd)
+        {
+            var process = new System.Diagnostics.Process
+            {
+                StartInfo = new System.Diagnostics.ProcessStartInfo
+                {
+                    WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden,
+                    FileName = "cmd.exe",
+                    Arguments = cmd
+                }
+            };
+            process.Start();
+            process.WaitForExit();
+            return process.ExitCode;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            SaveNetworkConnections();
+        }
+
+        [Serializable]
+        public class NetworkConnection
+        {
+            public string FolderPath { get; set; }
+            public string Username { get;  set; }
+            public string Password { get; set; }
+
+            public NetworkConnection(string folderPath, string username, string password)
+            {
+                FolderPath = folderPath;
+                Username = username;
+                Password = password;
+            }
+
+            public override string ToString()
+            {
+                return $"{FolderPath} ({Username})";
+            }
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
